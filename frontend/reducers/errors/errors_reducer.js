8425ebe8e212cc95d8720649d053889a6c3efd7c
elena churilov <elena.churilov@gmail.com>
2018-10-09 19:14:50 -0700

    Create Errors reducer

---
+++ frontend/reducers/errors/errors_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import userErrorsReducer from './user_errors_reducer';
+
+const errorsReducer = combineReducers({
+  userErrors: userErrorsReducer,
+});
+
+export default errorsReducer;
