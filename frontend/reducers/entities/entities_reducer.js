14f38501844d16eca3ffde7b67f1cf3401b0e7d4
elena churilov <elena.churilov@gmail.com>
2018-10-10 16:31:55 -0700

    Update Entities and Errors reducers

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import sessionsReducer from './sessions_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
+  sessions: sessionsReducer,
 });
 
 export default entitiesReducer;
