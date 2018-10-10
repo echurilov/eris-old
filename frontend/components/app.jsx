b7ce146efb681430b59c5370f280f8831f4802a9
elena churilov <elena.churilov@gmail.com>
2018-10-09 19:40:20 -0700

    Create Registration Form Container component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
-import RegistrationFormContainter from './user_form/registration_form_container';
+import RegistrationFormContainter from './registration_form/registration_form_container';
 
 cont App = () => (
   <div>
---
+++ frontend/components/registration_form/registration_form_container.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { register } from '../../actions/user_actions';
+import RegistrationForm from './registration_form';
+
+const mapStateToProps = ({ errors }) => ({
+  errors: errors.userErrors,
+  formType: 'register'
+});
+
+const mapDispatchToProps = dispatch => ({
+  processFrom: (user) => dispatch(register(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegistrationForm);
