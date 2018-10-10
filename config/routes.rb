6999700e062af64247aa6029ca9760a359fe0218
elena churilov <elena.churilov@gmail.com>
2018-10-10 09:04:17 -0700

    Start work on views and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: json} do
+  namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
