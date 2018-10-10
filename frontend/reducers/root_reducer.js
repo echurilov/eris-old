a95756990bccbd390b6449fe61f6cd6d017d6405
elena churilov <elena.churilov@gmail.com>
2018-10-09 19:16:45 -0700

    Create Root reducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities/entities_reducer';
+import errorsReducer from './errors/errors_reducer';
+
+const rootReducer = combineReducers({
+  entities: entitiesReducer,
+  errors: errorsReducer,
+});
+
+export default rootReducer;
