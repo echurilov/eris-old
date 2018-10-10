10b507c32991f2f4f14371e1780fb300c886535d
elena churilov <elena.churilov@gmail.com>
2018-10-10 11:04:33 -0700

    Removed Registration Container component and updated references

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
-import RegistrationContainer from './user/registration/registration_container';
+import Registration from './user/registration';
 import UserHome from './user/home';
 
 const App = () => (
   <div>
     <Switch>
-      <Route path="/register" component={RegistrationContainer} />
+      <Route path="/register" component={Registration} />
       <Route path="/channels/@me" component={UserHome} />
     </Switch>
   </div>
