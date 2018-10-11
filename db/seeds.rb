79a6da7d81b901ac5c0d9ca3afeb07c1b54987e8
elena churilov <elena.churilov@gmail.com>
2018-10-10 19:33:27 -0700

    Add demoUser to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+demoUser = User.create({"username"=>"Athena", "email"=>"athena@pantheon.io", "password"=>"pallas"})
