1efa22cbeb4af99bb31acc69876ff1680cc5bdbb
elena churilov <elena.churilov@gmail.com>
2018-10-15 14:47:09 -0700

    Create Memberships controller

--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -2,7 +2,7 @@ class Api::MembershipsController < ApplicationController
   def create
     @membership = Membership.new
     @membership.member_id = current_user.id
-    @member_id.server_id = params[:server_id]
+    @member_id.server_id = params[:id]
     if @membership.save
       render 'api/servers/show'
     else
@@ -18,5 +18,5 @@ class Api::MembershipsController < ApplicationController
       render json: {'server': ['Cannot leave a server you are not a member of']}
     end
   end
-  
+
 end
