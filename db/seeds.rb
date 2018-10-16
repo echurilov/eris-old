0c766de2a7baf6c8d8173c49e45b5957ef28d74b
elena churilov <elena.churilov@gmail.com>
2018-10-16 14:51:53 -0700

    Seed database with Servers and Memberships

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
+Server.delete_all
+Membership.delete_all
 
+zeus = User.create({"username"=>"Zeus", "email"=>"zeus@pantheon.io", "password"=>"ganymede"})
 athena = User.create({"username"=>"Athena", "email"=>"athena@pantheon.io", "password"=>"pallas"})
+hermes = User.create({"username"=>"Hermes", "email"=>"hermes@pantheon.io", "password"=>"caduceus"})
+artemis = User.create({"username"=>"Artemis", "email"=>"artemis@pantheon.io", "password"=>"bowandarrow"})
+apollo = User.create({"username"=>"Apollo", "email"=>"apollo@pantheon.io", "password"=>"chariot"})
+hades = User.create({"username"=>"Hades", "email"=>"hades@pantheon.io", "password"=>"cerberus"})
+persephone = User.create({"username"=>"Persephone", "email"=>"persephone@pantheon.io", "password"=>"sixseeds"})
+poseidon = User.create({"username"=>"Poseidon", "email"=>"poseidon@pantheon.io", "password"=>"stormyseas"})
+chronos = User.create({"username"=>"Chronos", "email"=>"chronos@pantheon.io", "password"=>"ticktock"})
+
+
+pantheon = Server.create("name" => "Pantheon", "owner_id" => chronos.id)
+  Membership.create("member_id" => chronos.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => zeus.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => poseidon.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => hades.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => athena.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => artemis.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => apollo.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => hermes.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => persephone.id, "server_id" => pantheon.id)
+
+olympus = Server.create("name" => "Olympus", "owner_id" => zeus.id)
+  Membership.create("member_id" => zeus.id, "server_id" => olympus.id)
+  Membership.create("member_id" => hades.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => poseidon.id, "server_id" => pantheon.id)
 
 athens = Server.create("name" => "Athens", "owner_id" => athena.id)
-
-Membership.create("member_id" => athena.id, "server_id" => athens.id)
+  Membership.create("member_id" => athena.id, "server_id" => athens.id)
+  Membership.create("member_id" => artemis.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => apollo.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => hermes.id, "server_id" => pantheon.id)
+  Membership.create("member_id" => persephone.id, "server_id" => pantheon.id)
