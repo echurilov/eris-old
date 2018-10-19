0e20cf1d62d86532d3bc0b16fea8e87b780760b1
elena churilov <elena.churilov@gmail.com>
2018-10-19 12:21:34 -0700

    Fix typo in Users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    user.home = Server.create('owner_id': @user.id, 'name': 'Home')
+    @user.home = Server.create('owner_id': @user.id, 'name': 'Home')
     if @user.save
       login!(@user)
       render 'api/users/show'
